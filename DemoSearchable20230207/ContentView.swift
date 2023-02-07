6682456c185e9f6f5bb08ad824f20781b356dc26
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-07 15:52:41 -0500

    Initial Commit

---
+++ DemoSearchable20230207/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DemoSearchable20230207
+//
+//  Created by Xavier on 2/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoSearchable20230207/DemoSearchable20230207App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSearchable20230207App.swift
+//  DemoSearchable20230207
+//
+//  Created by Xavier on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoSearchable20230207App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
